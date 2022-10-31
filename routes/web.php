be3b8c522615eb5141bab4fba38ae04215d83dae
mohammedterfa <mohammedterfa@gmail.com>
2022-10-31 15:18:35 +0200

    make root page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('book.index');
 });
 
 Route::get('/books','BookController@index')->name('book.index');
